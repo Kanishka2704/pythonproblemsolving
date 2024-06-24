621a050509fd3643653aa41021fbb434a851b955
Kanishka N <118846341+Kanishka2704@users.noreply.github.com>
2024-06-24 12:04:13 +0530

    Add files via upload
    Problem solving - 24-06-2024

---
+++ even_index.py
@@ -0,0 +1,46 @@
+#python - p t o
+
+user_text = input("Enter a text: ")
+
+for i in range(0,len(user_text),2):
+     print(user_text[i])
+
+#list slicing [0:3:2]
+#string slicing
+
+# city = "Bangalore"
+# print(city[0:5:2])#positive slicing
+# print(city[: : ]
+# print(city[-1])
+# print(city[ : :-1]) #negative slicing
+
+# #slicing:start-stop-step
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
---
+++ number_divisiblity_by_five.py
@@ -0,0 +1,7 @@
+#display number if Divisible by 5 from list
+
+list1 = [10,20,30,23,24,46,78]
+for i in list1:
+    if(i%5==0):
+        print(i)
+
